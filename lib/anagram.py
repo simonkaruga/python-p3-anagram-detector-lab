2eaa5e71ae8597aca11a7e4a6188a7f99353f0a8
simonkaruga <simonkaruganjoroge@gmail.com>
2025-11-24 14:19:29 +0300

    Add origin

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = ''.join(sorted(self.word))
+
+    def match(self, word_list):
+        matches = []
+        for w in word_list:
+            if w.lower() == self.word:
+                continue  # Skip if exact match, anagram shouldn't include itself?
+            if ''.join(sorted(w.lower())) == self.sorted_word:
+                matches.append(w)
+        return matches
